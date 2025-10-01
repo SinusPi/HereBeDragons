8e35e304221fc94f3ce48660503d181ff156da4c
Sinus <sinus@sinpi.net>
2025-10-01 02:51:20 +0200

    missed a ver number jog

--- HereBeDragons-Pins-2.0.lua
+++ HereBeDragons-Pins-2.0.lua
@@ -1,6 +1,6 @@
 -- HereBeDragons-Pins is a library to show pins/icons on the world map and minimap
 
-local MAJOR, MINOR = "HereBeDragons-Pins-ZGV", 11
+local MAJOR, MINOR = "HereBeDragons-Pins-ZGV", 15
 assert(LibStub, MAJOR .. " requires LibStub")
 
 local pins, oldversion = LibStub:NewLibrary(MAJOR, MINOR)
